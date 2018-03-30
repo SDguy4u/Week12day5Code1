a091f408e5faf5760550895f99cd7dca3f0e0917
SDguy4u <33709909+SDguy4u@users.noreply.github.com>
2018-03-30 23:14:36 +0530

    Add files via upload

---
+++ Week12day5Code1/src/Alphabets.java
@@ -0,0 +1,19 @@
+
+
+
+
+	import java.util.*;
+
+	public class Alphabets {
+
+	public static void main (String[] args) {
+	List <String> Name =  new ArrayList();
+	Name.add("Brother");
+	Name.add("Saksham");
+	Name.add("Sakshu");
+
+	Collections.sort(Name);
+	System.out.println(" The sorted data is " + Name);
+	}
+	}
+
